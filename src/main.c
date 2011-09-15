92653f55cedfef655b9ae819d8db1000d652f7d8
Alessandro Ghedini <al3xbio@gmail.com>
2011-09-15 10:28:55 +0200

    move db_{load,sync}() calls to cmd_* functions

--- src/main.c
+++ src/main.c
@@ -44,10 +44,10 @@
 #include "interface.h"
 #include "security.h"
 
-static inline void cmd_add(db_t *db, const char *arg);
-static inline void cmd_show(db_t *db, const char *arg);
-static inline void cmd_remove(db_t *db, const char *arg);
-static inline void cmd_dump(db_t *db);
+static inline void cmd_add(const char *arg);
+static inline void cmd_show(const char *arg);
+static inline void cmd_remove(const char *arg);
+static inline void cmd_dump();
 
 static inline char *get_db_path();
 
@@ -63,35 +63,28 @@ static struct option long_options[] = {
 #define DB_FILE "safely.db"
 
 int main(int argc, char *argv[]) {
-	db_t *db;
-	char *db_path;
 	int opts, i = 0;
 
 	security_check();
 
-	db_path = get_db_path();
-	db = db_load(db_path);
-
 	opts = getopt_long(argc, argv, "asrld", long_options, &i);
 
 	switch (opts) {
-		case 'a': { cmd_add(db, optarg);	break; }
-		case 's': { cmd_show(db, optarg);	break; }
-		case 'r': { cmd_remove(db, optarg);	break; }
-		case 'd': { cmd_dump(db);		break; }
+		case 'a': { cmd_add(optarg);	break; }
+		case 's': { cmd_show(optarg);	break; }
+		case 'r': { cmd_remove(optarg);	break; }
+		case 'd': { cmd_dump();		break; }
 
 		default: { }
 	}
 
-	db_sync(db, db_path);
-	db_unload(db);
-
-	free(db_path);
-
 	return 0;
 }
 
-static inline void cmd_add(db_t *db, const char *arg) {
+static inline void cmd_add(const char *arg) {
+	char *db_path = get_db_path();
+	db_t *db = db_load(db_path);
+
 	char usr[INPUT_MAX_SIZE], pwd[INPUT_MAX_SIZE];
 
 	printf("Enter user name  [%s]: ", arg);
@@ -105,26 +98,55 @@ static inline void cmd_add(db_t *db, const char *arg) {
 
 	item_add(db, arg, usr, pwd);
 	ok_printf("Added new item");
+
+	db_sync(db, db_path);
+	db_unload(db);
+
+	free(db_path);
 }
 
-static inline void cmd_show(db_t *db, const char *arg) {
+static inline void cmd_show(const char *arg) {
+	char *db_path = get_db_path();
+	db_t *db = db_load(db_path);
+
 	/* TODO: implement better show command */
 	const char *usr = item_get_usr(db, arg);
 	const char *pwd = item_get_pwd(db, arg);
 
 	ok_printf("user: %s, password: %s", usr, pwd);
+
+	db_sync(db, db_path);
+	db_unload(db);
+
+	free(db_path);
 }
 
-static inline void cmd_remove(db_t *db, const char *arg) {
+static inline void cmd_remove(const char *arg) {
+	char *db_path = get_db_path();
+	db_t *db = db_load(db_path);
+
 	item_remove(db, arg);
 	ok_printf("Removed item");
+
+	db_sync(db, db_path);
+	db_unload(db);
+
+	free(db_path);
 }
 
-static inline void cmd_dump(db_t *db){
+static inline void cmd_dump(){
+	char *db_path = get_db_path();
+	db_t *db = db_load(db_path);
+
 	const char *dump = db_dump(db);
 
 	puts(dump);
 	free((void *) dump);
+
+	db_sync(db, db_path);
+	db_unload(db);
+
+	free(db_path);
 }
 
 static inline char *get_db_path() {
